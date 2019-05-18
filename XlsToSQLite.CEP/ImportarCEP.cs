bb6ef8dd6f064921696b76faa8cdc596abb0ff41
Andre Mesquita <avmesquita@gmail.com>
2019-05-18 15:23:42 -0300

    Conversion tested

---
+++ XlsToSQLite.CEP/ImportarCEP.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using NPOI.OpenXml4Net;
+using NPOI.OpenXmlFormats.Spreadsheet;
+using NPOI.XSSF.UserModel;
+
+namespace ImportacaoCEP
+{
+	public class ImportarCEP
+	{
+		private ISheet GetFileStream(string fullFilePath)
+		{
+			var fileExtension = Path.GetExtension(fullFilePath);
+			string sheetName;
+			ISheet sheet = null;
+			switch (fileExtension)
+			{
+				case ".xlsx":
+					using (var fs = new FileStream(fullFilePath, FileMode.Open, FileAccess.Read))
+					{
+						var wb = new XSSFWorkbook(fs);
+						sheetName = wb.GetSheetAt(0).SheetName;
+						sheet = (XSSFSheet)wb.GetSheet(sheetName);
+					}
+					break;
+				case ".xls":
+					using (var fs = new FileStream(fullFilePath, FileMode.Open, FileAccess.Read))
+					{
+						var wb = new HSSFWorkbook(fs);
+						sheetName = wb.GetSheetAt(0).SheetName;
+						sheet = (HSSFSheet)wb.GetSheet(sheetName);
+					}
+					break;
+			}
+			return sheet;
+		}
+
+		public DataTable Importar(string fullFilePath)
+		{
+			try
+			{
+				var sh = GetFileStream(fullFilePath);
+				var dtExcelTable = new DataTable();
+				dtExcelTable.Rows.Clear();
+				dtExcelTable.Columns.Clear();
+				var headerRow = sh.GetRow(0);
+				int colCount = headerRow.LastCellNum;
+				for (var c = 0; c < colCount; c++)
+					dtExcelTable.Columns.Add(headerRow.GetCell(c).ToString());
+				var i = 1;
+				var currentRow = sh.GetRow(i);
+				while (currentRow != null)
+				{
+					var dr = dtExcelTable.NewRow();
+					for (var j = 0; j < currentRow.Cells.Count; j++)
+					{
+						var cell = currentRow.GetCell(j);
+
+						if (cell != null)
+							switch (cell.CellType)
+							{
+								case CellType.Numeric:
+									dr[j] = DateUtil.IsCellDateFormatted(cell)
+										? cell.DateCellValue.ToString(CultureInfo.InvariantCulture)
+										: cell.NumericCellValue.ToString(CultureInfo.InvariantCulture);
+									break;
+								case CellType.String:
+									dr[j] = cell.StringCellValue;
+									break;
+								case CellType.Blank:
+									dr[j] = string.Empty;
+									break;
+							}
+					}
+					dtExcelTable.Rows.Add(dr);
+					i++;
+					currentRow = sh.GetRow(i);
+				}
+				return dtExcelTable;
+			}
+			catch (Exception e)
+			{
+				throw;
+			}
+		}
+
+
+	}
+}
---
+++ XlsToSQLite.CEP/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using System.Data;
+using System.Data.SQLite;
+
+namespace ImportacaoCEP
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			#region [ LOAD EXCEL FILE ]
+
+			var arquivo = ".\\Data\\CEPs.xlsx";
+			var tabela = new ImportarCEP().Importar(arquivo);
+			var linhas = tabela.AsEnumerable().ToArray();
+
+			Console.WriteLine("XLS LINES FOUND = " + linhas.Count().ToString());
+
+			#endregion
+
+			#region [ CONFIGURE SQLITE ]
+			string sqlitedb = @".\cep.db3";
+
+			SQLiteConnectionStringBuilder connSB = new SQLiteConnectionStringBuilder();
+			connSB.DataSource = sqlitedb;
+			connSB.FailIfMissing = false;
+			connSB.Version = 3;
+			connSB.LegacyFormat = true;
+			connSB.Pooling = true;
+			connSB.JournalMode = SQLiteJournalModeEnum.Persist;
+
+			SQLiteConnection sqlite = new SQLiteConnection(connSB.ConnectionString);			
+			#endregion
+
+			#region [ SQLite INSERT Routine ]
+
+			string sql = string.Empty;
+
+			long execucaoLite = 0;
+			sqlite.Open();
+			foreach (var row in linhas)
+			{
+				sql = string.Format("INSERT INTO cep (TXT_CEP, TXT_CIDADE_UF, TXT_BAIRRO, TXT_LOCALIDADE) VALUES ('{0}','{1}','{2}','{3}') ", clean(row[0].ToString()), clean(row[1].ToString()), clean(row[2].ToString()), clean(row[3].ToString()));
+
+				var cmdLite = new SQLiteCommand(sql, sqlite);
+
+				execucaoLite += cmdLite.ExecuteNonQuery();
+			}			
+
+			sqlite.Close();
+
+			Console.WriteLine("SQLite Imported = " + execucaoLite.ToString());
+
+			#endregion
+		}
+
+		private static string clean(string texto)
+		{
+			texto = texto.Trim().Replace("'", "´");
+
+			return texto;
+		}
+
+	}
+}
